20b927b1c387c0957b1730549a24c73bacec1e7f
sworc <lightbox@nifty.com>
2024-05-06 19:09:21 +0900

    Add files via upload

---
+++ js-zoom.php
@@ -0,0 +1,84 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>zoomooz.js sample</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/zoomooz/1.1.6/jquery.zoomooz.min.js"></script>
+</head>
+<body>
+<h1 class="alert alert-primary">zoomooz.js</h1>
+<div id="main" class="m-4">
+    
+<table>
+<tr>
+<td>
+<img
+  id="img"
+  class="zoomTarget"
+  src="sample1.jpg"
+  style="border: solid 0px #000000;width:300px;"
+  data-targetsize="0.5"
+  data-closeclick="true"
+>
+</td><td>
+<img
+  id="img"
+  class="zoomTarget"
+  src="sample2.jpg"
+  style="border: solid 0px #000000;width:300px;"
+  data-targetsize="0.5"
+  data-closeclick="true"
+>
+</td>
+</tr>
+
+<tr>
+<td>
+
+<div style='margin:10px; width:250px;'
+  class="zoomTarget"
+  data-targetsize="0.8"
+  data-closeclick="true"
+>
+ある日の事でございます。御釈迦様は極楽の
+蓮池のふちを、独りでぶらぶら御歩きになっ
+ていらっしゃいました。池の中に咲いている
+蓮の花は、みんな玉のようにまっ白で、その
+まん中にある金色の蕊からは、何とも云えな
+い好い匂が、絶間なくあたりへ溢れて居りま
+す。極楽は丁度朝なのでございましょう。
+
+
+</div>
+
+</td><td>
+
+<div style='margin:10px; width:250px;'
+  class="zoomTarget"
+  data-targetsize="0.8"
+  data-closeclick="true"
+>
+やがて御釈迦様はその池のふちに御佇みにな
+って、水の面を蔽っている蓮の葉の間から、
+ふと下の容子を御覧になりました。この極楽
+の蓮池の下は、丁度地獄の底に当って居りま
+すから、水晶のような水を透き徹して、三途
+の河や針の山の景色が、丁度覗き眼鏡を見る
+ように、はっきりと見えるのでございます。
+
+
+
+</div>
+
+
+</td>
+</tr>
+</table>
+
+</div>
+</body>
+</html>
